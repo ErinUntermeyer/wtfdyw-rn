c0a1e1eaa14361fcd28edd22b20b2565e5f42f63
Erin Untermeyer <aerinuntermeyer@gmail.com>
2021-03-15 11:35:55 -0600

    Remove unused code

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
+import React from 'react'
 import Header from './Header'
 
 const App = () => {
   return (
     <Header />
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
-  container: {
-    // backgroundColor: 'white',
-    flex: 1,
-  }
-});
-
-export default App;
+export default App
